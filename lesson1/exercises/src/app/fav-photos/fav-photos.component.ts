6e6abca1eef737b17f2e1245abe0cc15f1f9ac2e
Chidera Obinali <chideraobinali1@gmail.com>
2020-09-23 15:48:34 -0500

    Completed parts 1 & 2

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://ixxidesign.azureedge.net/media/2401862/ixxi-sacre-e-frangine_coussi-cousine.jpg?width=498';
+  image3 = 'https://www.stylerave.com/wp-content/uploads/2020/06/sacree_frangine_79378803_109790670389136_8378056134395888714_n.jpg';
 
   constructor() { }
 
